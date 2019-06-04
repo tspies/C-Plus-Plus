fd99a197bdfde74f62d838f000a1ba99f513f552
Tristyn SPIES <tspies@c2r2s5.wethinkcode.co.za>
2019-06-04 14:57:28 +0200

    graphic fix, starting core phonebook functionality

--- WTC_Bootcamp/Day00/ex01/phonebook.cpp
+++ WTC_Bootcamp/Day00/ex01/phonebook.cpp
@@ -5,17 +5,18 @@
 int main(void)
 {
     std::string command;
-    std::cout << "Welcome to:\n\n";
+    std::cout << "\nWELCOME TO:\n\n";
  
- std::cout << " _____        _         _                   _        _____   _                              _                     _  \n";  
+std::cout << " _____        _         _                   _        _____   _                              _                     _  \n";  
 std::cout << "|_   _|      (_)       | |                 ( )      | ___ \\ | |                            | |                   | |  \n"; 
 std::cout << "  | |   _ __  _   ___  | |_   _   _  _ __  |/  ___  | |_/ / | |__     ___    _ __     ___  | |__    ___    ___   | | __\n";
 std::cout << "  | |  |  __|| | / __| | __| | | | ||  _  \\  / __|  |  __/  |  _ \\   / _ \\  |  _ \\   / _ \\ |  _ \\  / _ \\  / _ \\  | |/ /\n";
 std::cout << "  | |  | |   | | \\__ \\ | |_  | |_| || | | |  \\__ \\  | |     | | | | | (_) | | | | | |  __/ | |_)| | (_) || (_) | |   < \n";
 std::cout << "  \\_/  |_|   |_| |___/  \\__|  \\__  ||_| |_|  |___/  \\_|     |_| |_|  \\___/  |_| |_|  \\___| |____/  \\___/  \\___/  |_|\\_\\\n";
 std::cout << "                               __/ | \n";                                                                           
-std::cout << "                              |___/  \n";                                                                          
-                                                            
+std::cout << "                              |___/  \n\n";                                                                          
+
+    std::cout << "Please enter one of the following commands:\n";                                                        
     std::cout << "'ADD'- For a new contact\n";
     std::cout << "'SEARCH'- To search for an existing contact\n";
     std::cout << "'EXIT'- To quit the program\n";
